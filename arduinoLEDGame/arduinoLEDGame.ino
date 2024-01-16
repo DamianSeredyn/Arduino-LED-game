19e69efcd55a6772d3be35f3240aa91958050338
DamianSeredyn <62287667+DamianSeredyn@users.noreply.github.com>
2024-01-16 21:43:42 +0100

    Update arduinoLEDGame.ino

--- arduinoLEDGame/arduinoLEDGame.ino
+++ arduinoLEDGame/arduinoLEDGame.ino
@@ -1,9 +1,76 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const int LedPinStart = 8;
+const int LedPinEnd = 11;
+
+unsigned long actualTime = 0;
+unsigned long currentTime = 0;
+unsigned long LedTime = 500;
+
+unsigned int numberOfLeds = 5;
+unsigned int index;
+int LEDorder[5];
+
+bool IdleMode = false;
+bool GamePlaying = false;
+bool showLed = false;
+void setup() {
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  for(int i = LedPinStart; i<LedPinEnd;i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+  InitGame();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  actualTime = millis();
+  
+  if(GamePlaying)
+  {
+    if(showLed)
+    {
+        if(actualTime - currentTime >= LedTime)
+        {
+          digitalWrite(LEDorder[index],LOW);
+          if(numberOfLeds<index)
+          {
+            showLed = false;
+          }
+          else if(actualTime - currentTime >= LedTime+75)
+          {
+            index++;
+            digitalWrite(LEDorder[index],HIGH);
+            currentTime = actualTime;
+          }
+        }
+    }
+    else
+    {
 
+    }
+  }
+  
+  if(IdleMode)
+  {
+
+  }
 }
+
+
+void InitGame()
+{
+    for(int i=0;i<numberOfLeds;i++)
+    {
+      LEDorder[i] = random(LedPinStart,LedPinEnd+1);
+      Serial.println(LEDorder[i]);
+    }
+    showLed = true;
+    GamePlaying = true;
+    index = 0;
+    currentTime = actualTime;
+    digitalWrite(LEDorder[index],HIGH);
+}
+
+
+
