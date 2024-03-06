d18ad1dd6e5a9cc72abecd8ad99ca26251b7297b
DamianSeredyn <62287667+DamianSeredyn@users.noreply.github.com>
2024-03-07 00:32:46 +0100

    Add win condition

--- arduinoLEDGame/arduinoLEDGame.ino
+++ arduinoLEDGame/arduinoLEDGame.ino
@@ -4,26 +4,31 @@ const int LedPinEnd = 11;
 const int buttonOut = 12;
 const int buttonIn = 2;
 
+const int loseDiode = 7;
+const int winDiode = 13;
 
 unsigned long actualTime = 0;
 unsigned long currentTime = 0;
 unsigned long buttonTime = 0;
+unsigned long EndLEDTime = 0;
 unsigned long LedTime = 500;
 unsigned long lightime = 0;
 
 unsigned long currentPressed = 0;
 
 unsigned int numberOfLeds = 5;
+unsigned int playerProgress = 0;
 unsigned int index;
 int LEDorder[5];
 
 bool IdleMode = false;
 bool GamePlaying = false;
+bool GameFinished = false;
 bool showLed = false;
 void setup() {
   Serial.begin(9600);  
   randomSeed(analogRead(0));
-  for(int i = LedPinStart; i<=LedPinEnd;i++)
+  for(int i = LedPinStart; i<=13;i++)
   {
     pinMode(i, OUTPUT);
   }
@@ -39,7 +44,6 @@ void setup() {
 
 void loop() {
   actualTime = millis();
-  Serial.println(digitalRead(2));
   if(GamePlaying)
   {
     if(showLed)
@@ -61,9 +65,9 @@ void loop() {
     }
     else
     {
-      if(actualTime - buttonTime >= 50)
+      if(actualTime - buttonTime >= 25)
       {
-        if(actualTime - lightime >= 200)
+        if(actualTime - lightime >= 250)
         {
           digitalWrite(currentPressed,LOW);
           buttonTime = actualTime;
@@ -73,24 +77,50 @@ void loop() {
         
         if(!digitalRead(currentPressed) && digitalRead(currentPressed-5))
         {
-          currentPressed =0;
+         currentPressed =0;
         }
           for(int i = 3; i<=6;i++)
           {
             if(!digitalRead(i) && currentPressed != i+5)
             {
+              Serial.println("klik");
               digitalWrite(i+5,HIGH);
               currentPressed = i+5;
               lightime = actualTime;
-              Serial.println(i+5);
-              break;
+              if(playerProgress == numberOfLeds || LEDorder[playerProgress] != currentPressed)
+              {
+                GameFinished = true;
+                GamePlaying = false;
+                for(int i = LedPinStart; i<=LedPinEnd;i++)
+                {
+                  digitalWrite(i, HIGH);
+                }
+                EndLEDTime = actualTime;
+                if(playerProgress == numberOfLeds)
+                  digitalWrite(winDiode, HIGH);
+                else
+                  digitalWrite(loseDiode, HIGH);
+              } 
+              playerProgress++;            
             }
-          }
-
+            }
       }
     }
   }
-  
+  if(GameFinished)
+  {
+    if(actualTime - EndLEDTime >= 500)
+    {
+      GameFinished = false;
+      for(int i = LedPinStart; i<=LedPinEnd;i++)
+      {
+        digitalWrite(i, LOW);
+      }
+      digitalWrite(winDiode, LOW);
+      digitalWrite(loseDiode, LOW);
+    }     
+    
+  }
   if(IdleMode)
   {
 
@@ -104,20 +134,21 @@ void InitGame()
     {
       randomSeed(analogRead(0));
       LEDorder[i] = random(LedPinStart,LedPinEnd+1);
-      Serial.println(LEDorder[i]);
     }
     showLed = true;
     GamePlaying = true;
     index = 0;
     currentTime = actualTime;
+    playerProgress = 0;
     digitalWrite(LEDorder[index],HIGH);
 }
 
 void StartGame()
 {
-    if(!GamePlaying)
-    InitGame();
+    if(!GamePlaying && !GameFinished)
+      InitGame();
 }
+
 
 
 
